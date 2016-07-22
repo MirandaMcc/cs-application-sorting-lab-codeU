ac4f7ec37f1255e431379dbf83c2e89f105bff73
Miranda <mcclm@mit.edu>
2016-07-22 01:05:19 -0700

    non-recurisve mergeSort works

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -9,6 +9,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Collections;
 
 /**
  * Provides sorting algorithms.
@@ -64,7 +65,44 @@ public class ListSorter<T> {
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
         // FILL THIS IN!
-        return null;
+		//simple base case
+		if (list.size() <= 1)
+			return list;
+
+		//split list in half
+		List<T> list1 = new LinkedList<T>(list.subList(0,list.size()/2));
+		List<T> list2 = new LinkedList<T>(list.subList(list.size()/2,list.size()));
+
+		//Sort the halves using Collections.sort or insertionSort
+		Collections.sort(list1,comparator);
+		Collections.sort(list2,comparator);
+
+		System.out.println("list1:"+list1);
+		System.out.println("list2:"+list2);
+		//Merge the sorted halves into a complete sorted list
+		List<T> sorted = merge(list1,list2,comparator);
+		System.out.println("list:"+sorted);
+        return sorted;
+	}
+
+	//helper method for merge functionality
+	private List<T> merge(List<T> list1, List<T> list2,Comparator<T> comparator){
+		List<T> sorted = new ArrayList<T>();
+		int sizeTotal = list1.size()+list2.size();
+		while(sorted.size() < sizeTotal){
+			if(list2.isEmpty()){
+				sorted.addAll(list1);
+			}
+			else if(list1.isEmpty()){
+				sorted.addAll(list2);
+			}
+			else if(comparator.compare(list2.get(0),list1.get(0)) <= 0) //less than
+					sorted.add(list2.remove(0)); //ineffitient removing from front, need to use linkedlists
+			else
+					sorted.add(list1.remove(0));
+			}
+			
+		return sorted;
 	}
 
 	/**
